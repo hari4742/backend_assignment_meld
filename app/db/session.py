735b7c9a09524b2ae42a140727ba895adaa4385b
HariKiran <harikiran.kante@gmail.com>
2025-01-21 20:18:45 +0530

    importing the models and updated db url

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./reviews.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app/db/reviews.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
                        "check_same_thread": False})
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.category import Category
+from app.models.review import ReviewHistory
+from app.models.access_log import AccessLog
+
+__all__ = ["Category", "ReviewHistory", "AccessLog"]
